e9ee2b822f1eaf8521649ed586e4dd5de67354bc
Bryan Daniel <>
2024-12-04 17:15:57 -0500

    just entered the start of the Johnson class

--- src/lab6/Johnson.java
+++ src/lab6/Johnson.java
@@ -1,6 +1,11 @@
 package lab6;
 
-public class Johnson
+// i think i'm going to have this class simply for the johnson method
+// that takes a graph reference as a parameter and just prints out the solution
+public class Johnson<T>
 {
-
+    public void Johnson(Graph<T> testGraph)
+    {
+        // add the logic here later
+    }
 }
